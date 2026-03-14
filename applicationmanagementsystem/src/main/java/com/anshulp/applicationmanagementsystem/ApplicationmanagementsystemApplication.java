805b3595851bf8b714ebf75f420a71438eb9958e
Anshul Pandey <pandeyanshul2004@gmail.com>
2026-03-14 08:50:48 +0530

    initial commit

---
+++ applicationmanagementsystem/src/main/java/com/anshulp/applicationmanagementsystem/ApplicationmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.anshulp.applicationmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationmanagementsystemApplication.class, args);
+	}
+
+}
---
+++ applicationmanagementsystem/src/test/java/com/anshulp/applicationmanagementsystem/ApplicationmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshulp.applicationmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
